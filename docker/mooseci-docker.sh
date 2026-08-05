5550483b8bdf8b7e31e84f4350848e7f300a82f6
tokyRT <tokyrt.pro@gmail.com>
2026-08-05 12:07:19 +0300

    fix init command not creating moose-ci.ston file

--- docker/mooseci-docker.sh
+++ docker/mooseci-docker.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
+cd /src 2>/dev/null || true
 
-
-
-./pharo --headless Moose13/Moose13.image --no-default-preferences clap moose-ci "$@"
+/pharo --headless /Moose13/Moose13.image --no-default-preferences clap moose-ci "$@"
