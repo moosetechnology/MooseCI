af321c8ef7b660b035203973e73e0760e5084179
tokyRT <tokyrt.pro@gmail.com>
2026-08-25 23:53:46 +0300

    add tests for java loader

---
+++ ressources/mock-project-tests/java-only/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Entry point of the mock project used by the Moose-CI tests.
+ */
+public class Main {
+
+    /**
+     * Runs the application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Moose");
+    }
+}
